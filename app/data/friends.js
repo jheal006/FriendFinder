1d9818ed628d1837f88816b4a7ae7482ad1712a2
jheal006 <jheal006@gmail.com>
2017-09-25 23:22:01 -0700

    FriendFinder project

---
+++ app/data/friends.js
@@ -0,0 +1,83 @@
+var friendsData = [
+    {
+    "name":"Alice",
+    "photo":"http://static.tvtropes.org/pmwiki/pub/images/alice1_large.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"Cheshire Cat",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/8/8e/DisneyCheshireCat.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"Queen of Hearts",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/1/1c/DisneyQueenHearts.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"Mad Hatter",
+    "photo":"http://www.animationsensations.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/m/a/mad-hatter2.jpg",
+    "scores":[
+        3,
+        2,
+        2,
+        4,
+        3,
+        2,
+        3,
+        4,
+        2,
+        2
+      ]
+  },
+  {
+    "name":"White Rabbit",
+    "photo":"https://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=11017832",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+]
+module.exports=friendsData;
---
+++ app/routes/apiRoutes.js
@@ -0,0 +1,70 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+
+// var tableData = require("../data/tableData");
+// var waitListData = require("../data/waitinglistData");
+var friendsData = require("../data/friends.js");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+  var bestMatch;
+  app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    var newFriend = req.body;
+        //Comparative Loop
+        // Loop through Existing Characters / Friends
+        var lowestScore;
+        var newScore = 1000;
+        for (var i = 0; i < friendsData.length-1; i++) {
+          var comparisonScore =  0;
+          var totalDifference = [];
+          // Loop through Individual Friend Scores
+            for (var j = 0; j < friendsData[i].scores.length; j++) {
+              totalDifference.push(Math.abs(newFriend.scores[j] - friendsData[i].scores[j]));
+            }
+          // Sum the totalDifference Scores to find that of the lowest number for the best match!
+            for (var k in totalDifference)
+            {
+              comparisonScore += totalDifference[k];
+            }
+          //Compare comparisonScores as they loop through to determine the lowest score.
+            console.log("FRIEND====", friendsData[i].name, "TOTAL DIFFERENCE====", totalDifference, "COMPARISON SCORE =====", comparisonScore);
+            if (comparisonScore < newScore) {
+              newScore = comparisonScore;
+              bestMatch = friendsData[i].name;
+            }
+        }
+        console.log("NEW SCORE RESULT====", newScore, "BEST MATCH=====", bestMatch);
+        //Push newly entered Friend info to FriendsData Array
+        friendsData.push(newFriend);
+
+        //Attempting to push the bestMatch info back to front end in order to properly display the modal.        
+        res.json(bestMatch);
+
+  });
+
+};
